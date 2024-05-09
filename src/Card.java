676656d5ca789434df314c3bb0f7198b1ac869d7
finnchen26 <148796107+finnchen26@users.noreply.github.com>
2024-05-09 09:05:02 -0400

    5/9

--- src/Card.java
+++ src/Card.java
@@ -7,25 +7,68 @@ public class Card {
     public String type;
 
 
-    public Card(int pType, int pSuit){
-        if(pType == 0){
+    public Card(int pType, int pSuit) {
+        if (pType == 0) {
             type = "Ace";
-        }
-        else if (pType == 1){
+            value = 1;
+        } else if (pType == 1) {
             type = "Two";
+            value = 2;
+        } else if (pType == 2) {
+            type = "Three";
+            value = 3;
+        } else if (pType == 3) {
+            type = "Four";
+            value = 4;
+        } else if (pType == 4) {
+            type = "Five";
+            value = 5;
+        } else if (pType == 5) {
+            type = "Six";
+            value = 6;
+        } else if (pType == 6) {
+            type = "Seven";
+            value = 7;
+        } else if (pType == 7) {
+            type = "Eight";
+            value = 8;
+        } else if (pType == 8) {
+            type = "Nine";
+            value = 9;
+        } else if (pType == 9) {
+            type = "Ten";
+            value = 10;
+        } else if (pType == 10) {
+            type = "Jack";
+            value = 10;
+        } else if (pType == 11) {
+            type = "Queen";
+            value = 10;
+        } else if (pType == 12) {
+            type = "King";
+            value = 10;
         }
-        if (pSuit == 1){
+
+
+        if (pSuit == 0) {
+            suit = "Diamonds";
+        }
+        if (pSuit == 1) {
             suit = "Spades";
         }
-
-
-
-        //TODO: write if statements that set the value of "value" based on type
-        //value =
+        if (pSuit == 2) {
+            suit = "Hearts";
+        }
+        if (pSuit == 3) {
+            suit = "Clubs";
+        }
     }
 
+
+
     public void print(){
-        System.out.println("The" + type + " of " + suit + " is worth " + value + " points.");
+        System.out.println("The " + type + " of " + suit + " is worth " + value + " points.");
 
     }
 }
+
--- src/Casino.java
+++ src/Casino.java
@@ -7,18 +7,46 @@ public class Casino {
 
     public Casino(){
         System.out.println("Welcome to Finn's Casino!");
+
+        makeDeck();
+        shuffle();
+        printDeck();
+
+
+    }
+
+    public void makeDeck(){
         deck = new Card[52];
         int count = 0;
 
         for(int t = 0; t < 13; t++){
             for(int s = 0; s < 4; s++){
-                if(t == 0){
-                    deck[count] = new Card(t, s);
-                    deck[count].print();
-                    count++;
-                }
+
+                deck[count] = new Card(t, s);
+                deck[count].print();
+                count++;
 
             }
+        }
+    }
+
+    public void shuffle(){
+        //for loop
+        for (int x = 0; x < 52; x++){
+            //grab rand card and swap with the current index (x)
+            int randomIndex = (int)(Math.random()*52);
+            Card randomCard = deck[randomIndex];
+            //swap
+            Card temp = deck[x];
+            deck[x] = randomCard;
+            deck[randomIndex] = temp;
+        }
+
+    }
+
+    public void printDeck(){
+        for (int i = 0; i < deck.length; i++){
+            deck[i].print();
         }
     }
 }
