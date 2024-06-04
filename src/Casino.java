8ac7ef5da8642e1f60ec007080c80a21b8c9d736
finnchen26 <148796107+finnchen26@users.noreply.github.com>
2024-06-04 12:36:59 -0400

    6/4

--- src/Casino.java
+++ src/Casino.java
@@ -12,78 +12,96 @@ public class Casino {
 
     public Casino() {
         System.out.println("Welcome to Finn's Casino!");
-
-        makeDeck();
-        shuffle();
-        printDeck();
-        dealer = new Player();
-        dealer.isPlayer = false;
-        p = new Player();
-        deal();
-        dealer.print();
-        p.print();
-        System.out.println(p.isPlayer);
-
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your name?");
         String userName = scan.nextLine();
         System.out.println(userName);
-        p.name = userName;
-        p.print();
 
-        String decision = "";
-        while (!decision.equals("stand") && p.cardsValue <= 21) {
-            System.out.println();
-            System.out.println("Player, Do you want to stand or hit?");
-            decision = scan.nextLine();
-            System.out.println(decision);
-            boolean isHit;
-            if (decision.equals("hit")) {
-                isHit = true;
-                System.out.println("you have decided to hit");
-            } else {
-                isHit = false;
-                System.out.println("you have decided to stand");
-                p.isPlayer = false;
-            }
+        boolean isPlaying;
+        isPlaying = true;
+        while (isPlaying) {
 
-            if (isHit) {
-                //take a card from the deck, add it to something
-                //use add card to do this
-                //update value
-                p.addCard(deck[numDealtcards]);
-                numDealtcards++;
-                p.print();
-                if (p.cardsValue > 21){
-                    System.out.println("Sorry, BUST!!!");
-                }
+            makeDeck();
+            shuffle();
+            printDeck();
+            dealer = new Player();
+            dealer.isPlayer = false;
+            p = new Player();
+            deal();
+            dealer.print();
+            p.print();
+            System.out.println(p.isPlayer);
 
-            } else {
-              //  p.isPlayer = false;
-                System.out.println("Player on hold...Dealer decision in action");
-                while (dealer.cardsValue < 17) {
+
+            p.name = userName;
+            p.print();
+
+            String decision = "";
+            while (!decision.equals("stand") && p.cardsValue <= 21) {
+                System.out.println();
+                System.out.println("Player, Do you want to stand or hit?");
+                decision = scan.nextLine();
+                System.out.println(decision);
+                boolean isHit;
+                if (decision.equals("hit")) {
                     isHit = true;
-                    System.out.println("dealer has decided to hit");
-                    if (isHit){
-                        dealer.addCard(deck[numDealtcards]);
-                        numDealtcards++;
-                        dealer.print();
-                    }
+                    System.out.println("you have decided to hit");
+                } else {
+                    isHit = false;
+                    System.out.println("you have decided to stand");
+                    p.isPlayer = false;
                 }
+
+                if (isHit) {
+                    //take a card from the deck, add it to something
+                    //use add card to do this
+                    //update value
+                    p.addCard(deck[numDealtcards]);
+                    numDealtcards++;
+                    p.print();
+                    if (p.cardsValue > 21) {
+                        System.out.println("Sorry, BUST!!!");
+                    }
+
+                } else {
+                    //  p.isPlayer = false;
+                    System.out.println("Player on hold...Dealer decision in action");
+                    while (dealer.cardsValue < 17) {
+                        isHit = true;
+                        System.out.println("dealer has decided to hit");
+                        if (isHit) {
+                            dealer.addCard(deck[numDealtcards]);
+                            numDealtcards++;
+                            dealer.print();
+                        }
+                    }
                     isHit = false;
                     System.out.println("dealer has decided to stand");
                     System.out.println("The dealer has a final point of " + dealer.cardsValue);
                     System.out.println("Your final point is " + p.cardsValue);
 
-                    if (dealer.cardsValue < p.cardsValue){
+                    if (dealer.cardsValue < p.cardsValue) {
                         System.out.println("Congratulations! You have won!");
-                    }else{
+                    } else if (dealer.cardsValue == p.cardsValue) {
+                        System.out.println("You have tied with the dealer!");
+                    } else {
                         System.out.println("Sorry, better luck next time!");
                     }
 
+                }
+
+
             }
-
-
+            System.out.println("The round has ended. Play Again or Stop Game?");
+            String gameDecision = scan.nextLine();
+            System.out.println(gameDecision);
+            if (gameDecision.equals("Play Again")) {
+                isPlaying = true;
+                System.out.println("Yay! Play Again!");
+            } else {
+                isPlaying = false;
+                System.out.println("Stopping game. Come again next time!");
+            }
 
         }
     }
